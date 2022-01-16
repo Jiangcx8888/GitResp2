16107182c4a360fffc7d9fd01760937aec2154dd
jiangcx <Chinajcx@1108.com>
2022-01-16 16:30:34 +0800

    在Student类中增加了age，name属性

--- TestModule01/src/com/jcx/Student.java
+++ TestModule01/src/com/jcx/Student.java
@@ -1,10 +1,6 @@
 package com.jcx;
 
-/**
- * @Auther: jiangcx
- * @Date: 2022/1/16 - 01 - 16 - 16:22
- * @Description: com.jcx
- * @version: 1.0
- */
 public class Student {
+    private int age;
+    private String name;
 }
